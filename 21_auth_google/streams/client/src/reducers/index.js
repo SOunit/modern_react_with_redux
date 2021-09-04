a9b538c2e008b9c000f93aa648cb4c97ba2e218c
SOunit <workbookwork7@gmail.com>
2021-09-03 23:53:13 -0700

    add auth reducer

--- 21_auth_google/streams/client/src/reducers/index.js
+++ 21_auth_google/streams/client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
 export default combineReducers({
-  replaceMe: () => 'replaceMe reducer',
+  auth: authReducer,
 });
